18a0eced23376a45b1be803b78fd7c22b2c392a8
slycoder0 <slycoder0@proton.me>
2024-11-04 13:59:36 -0300

    Melhorias carrocel

--- src/components/ProdutosColecao/ProdutosColecao.jsx
+++ src/components/ProdutosColecao/ProdutosColecao.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from 'react-slick';
 import './ProdutosColecao.css';
 import { FaArrowRight } from "react-icons/fa";
 
@@ -10,6 +11,10 @@ import Esportivas from '../../assets/img/PlacasEsportivas.png';
 import Religiosa from '../../assets/img/PlacasReligiosas.png';
 import Gaiola from '../../assets/img/PlcasGaiola.png';
 
+// Importação do CSS do slick-carousel
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 export default function ProdutosColecao() {
   const produtos = [
     { id: 1, nome: 'Imãs Decorativos', imagem: king },
@@ -20,21 +25,45 @@ export default function ProdutosColecao() {
     { id: 6, nome: 'Placas de Gaiolas', imagem: Gaiola },
   ];
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
+
   return (
-<>
     <div className="produtos-colecao">
-      {produtos.map(produto => (
-        <div key={produto.id} className="produto-item">
-          <img src={produto.imagem} alt={produto.nome} className="produto-imagem" />
-          <div className="produto-info">
-            <h3 className="produto-nome">{produto.nome}</h3>
-            <a href="#" className="produto-link">
-              <FaArrowRight className="produto-seta" />
-            </a>
+      <Slider {...settings}>
+        {produtos.map(produto => (
+          <div key={produto.id} className="produto-item">
+            <img src={produto.imagem} alt={produto.nome} className="produto-imagem" />
+            <div className="produto-info">
+              <h3 className="produto-nome">{produto.nome}</h3>
+              <a href="#" className="produto-link">
+                <FaArrowRight className="produto-seta" />
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </Slider>
     </div>
-    </>
   );
 }
