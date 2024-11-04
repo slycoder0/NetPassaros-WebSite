a98e5000c4ef6043d0f6773306ef3bdad66278aa
slycoder0 <slycoder0@proton.me>
2024-11-04 13:20:27 -0300

    att home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,9 +2,6 @@ import Atacado from '../../components/Atacado/Atacado';
 import Banner from '../../components/Banner/Banner';
 import ProdutosColecao from '../../components/ProdutosColecao/ProdutosColecao';
 import SobreNos from '../../components/SobreNos/SobreNos';
-import './Homepage.css'; // Certifique-se de que o nome do arquivo e o caminho estão corretos
-
-
 
 export default function HomePage() {
   return (
