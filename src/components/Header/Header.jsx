1a4e315086b033755a2673418ff4672796d88828
slycoder0 <slycoder0@proton.me>
2024-11-04 13:42:09 -0300

    att

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,31 @@
-// Importação do CSS, logo e ícones
-import "./Header.css";
-import Logo from './logo2.png'; //  direto a partir da pasta public
-import { FaBagShopping, FaMagnifyingGlass } from "react-icons/fa6";
+import React, { useState } from 'react';
+import './Header.css';
+import Logo from './logo2.png'; // Caminho direto a partir da pasta public
+import { FaBagShopping, FaMagnifyingGlass, FaBars } from "react-icons/fa6";
 import { IoPerson } from "react-icons/io5";
 
-// Componente Header
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-      <nav className="navigation">
+      <div className="logo">
+        <img src={Logo} alt="Company Logo" />
+      </div>
+      <nav className={`navigation ${menuOpen ? 'open' : ''}`}>
         <a href="#" className="nav-link">Início</a>
         <a href="#" className="nav-link">Produtos</a>
         <a href="#" className="nav-link">Quem Somos</a>
       </nav>
-      <div className="logo">
-        <img src={Logo} alt="Company Logo" />
-      </div>
       <div className="icons">
         <IoPerson className="icon" />
         <FaMagnifyingGlass className="icon" />
         <FaBagShopping className="icon" />
+        <FaBars className="icon menu-icon" onClick={toggleMenu} />
       </div>
     </header>
   );
