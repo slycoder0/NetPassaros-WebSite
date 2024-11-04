5e2ba5fc443e360f43d70e29f67cc488833df821
slycoder0 <slycoder0@proton.me>
2024-11-04 12:45:39 -0300

    correcao footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css';
 import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
-import Logo from '../../../public/Logo2.png'; // Substitua pelo caminho correto da sua logo
+import Logo from '../../../public/logo2.png'; 
 
 export default function Footer() {
   return (
