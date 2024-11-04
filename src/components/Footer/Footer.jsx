a42a73ba4cbe75c08989857732ca2b2da2fa80be
slycoder0 <slycoder0@proton.me>
2024-11-04 13:06:12 -0300

    a

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import './Footer.css';
 import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
-import Logo from '/logo2.png'; // Caminho direto a partir da pasta public
-
+import Logo from '../../../public/logo2.png'; 
 
 export default function Footer() {
   return (
