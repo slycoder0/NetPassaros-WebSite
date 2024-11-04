91eb7e0f7f30241083b0046b0c61521a36984009
slycoder0 <slycoder0@proton.me>
2024-11-04 13:15:22 -0300

    header att

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 // Importação do CSS, logo e ícones
 import "./Header.css";
-import Logo from '/Logo2.png'; // Caminho direto a partir da pasta public
+import Logo from '/Logo2.png'; //  direto a partir da pasta public
 import { FaBagShopping, FaMagnifyingGlass } from "react-icons/fa6";
 import { IoPerson } from "react-icons/io5";
 
